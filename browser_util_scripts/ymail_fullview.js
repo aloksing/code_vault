1f10e1178b1958f64354f512c66f60d3c1e29973
Alok Singh <aloksing@yahoo-inc.com>
2015-02-24 23:57:33 +0530

    Browser Utility Scripts for Greasmonkey/Tampermonkey

--- browser_util_scripts/ymail_fullview.js
+++ browser_util_scripts/ymail_fullview.js
@@ -9,13 +9,13 @@
 // ==/UserScript==
 
 function addGlobalStyle(css) {
-	    var head, style;
-	        head = document.getElementsByTagName('head')[0];
-		    if (!head) { return; }
-		        style = document.createElement('style');
-			    style.type = 'text/css';
-			        style.innerHTML = css;
-				    head.appendChild(style);
+	var head, style;
+	head = document.getElementsByTagName('head')[0];
+	if (!head) { return; }
+        style = document.createElement('style');
+	style.type = 'text/css';
+        style.innerHTML = css;
+	head.appendChild(style);
 }
 
 addGlobalStyle('#shellcontent { right: 0px !important; }');
