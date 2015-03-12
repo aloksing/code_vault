b7ecc8a9320e22330b79c1cbf9b59d47084fd5fe
Alok Singh <reply2aloksingh@gmail.com>
2015-03-12 06:14:39 +0530

    Improved comments

--- browser_util_scripts/ymail_fullview.js
+++ browser_util_scripts/ymail_fullview.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name         Remove Y! Mail Side Ad Div
+// @name         ymail_fullview.js
 // @namespace    https://in.yahoo.com/
 // @version      0.1
-// @description  enter something useful
-// @author       You
+// @description  Removes the Y! Mail Right Rail Ad for a cleaner 
+// 		 viewing experience
+// @author       Alok Singh
 // @match        https://*.mail.yahoo.com/*
 // @grant none
 // ==/UserScript==
-// Simple Greasemonkey/Tampermonkey Script to remove the Right Side 
+// Simple Greasemonkey/Tampermonkey Script to remove the Right Rail 
 // Ads Pane in Yahoo Mail. Works for Firefox and Google Chrome.
 
 function addGlobalStyle(css) {
